535ab78afbdd89c7167d5dc60fda54f627ea9a56
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-27 18:28:56 -0400

    Lambton Mall 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -205,6 +205,8 @@ function renderFeatureItems(){
     var items = [];
     var images= ["http://via.placeholder.com/470x940","http://via.placeholder.com/470","http://via.placeholder.com/470","http://via.placeholder.com/470","http://via.placeholder.com/470x940","http://via.placeholder.com/470","http://via.placeholder.com/470","http://via.placeholder.com/470"]
     $.each(items, function(i, val){
+    });
+    $.each(items, function(i, val){
         if(Cookies.get('current_locale') == "en-CA"){
             $('#feature_' + i).html('<a href="'+ val.url +'"><img src="'+ val.image_url+'" class="hoverer" alt="' +val.name+ '"><h5 class="center_h">'+ val.name +'</h5></a>')
         } else if(Cookies.get('current_locale') == "fr-CA"){
