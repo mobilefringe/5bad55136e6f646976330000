a1f20be6558bf87af4be52f9f079d2c44cf7d4cb
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-28 17:21:18 -0400

    Lambton Mall 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -203,7 +203,7 @@ function renderEventDetails(container, template, collection, mall_name){
 function renderFeatureItems(){
     // var items = getFeatureList();
     var items = [];
-    var images= ["https://picsum.photos/470/940?image=1070","https://picsum.photos/470?image=1071","https://picsum.photos/470?image=1072","https://picsum.photos/470?image=1073","https://picsum.photos/470?image=1074","https://picsum.photos/470/940?image=1077","https://picsum.photos/470?image=1076"]
+    var images= ["https://picsum.photos/470/940?image=1070","https://picsum.photos/470?image=1071","https://picsum.photos/470?image=1072","https://picsum.photos/470?image=1073","https://picsum.photos/470?image=1074","https://picsum.photos/470?image=1076","https://picsum.photos/470/940?image=1077"]
     // "https://picsum.photos/470/940?image=1077"
     $.each(images, function(i, val){
         var item = {};
